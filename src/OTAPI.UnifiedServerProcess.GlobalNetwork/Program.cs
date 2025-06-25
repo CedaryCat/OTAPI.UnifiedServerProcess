e7faa064a7638d01ca965aeac4613635f025536c
CedaryCat <2503439479@qq.com>
2025-06-26 04:10:49 +0800

    style: title

--- src/OTAPI.UnifiedServerProcess.GlobalNetwork/Program.cs
+++ src/OTAPI.UnifiedServerProcess.GlobalNetwork/Program.cs
@@ -3,6 +3,7 @@ using OTAPI.UnifiedServerProcess.GlobalNetwork.Network;
 using OTAPI.UnifiedServerProcess.GlobalNetwork.Servers;
 using ReLogic.OS;
 using System.Diagnostics;
+using System.Reflection;
 using UnifiedServerProcess;
 
 namespace OTAPI.UnifiedServerProcess.GlobalNetwork
@@ -12,13 +13,15 @@ namespace OTAPI.UnifiedServerProcess.GlobalNetwork
         static void Main(string[] args) {
             var version = new VersionHelper();
 
-            Console.WriteLine("___________________________________________________________________________________________________");
-            Console.WriteLine("_  _ _  _ _ ____ _ ____ ___     ____ ____ ____ _  _ ____ ____    ___  ____ ____ ____ ____ ____ ____");
-            Console.WriteLine("|  | |\\ | | |___ | |___ |  \\    [__  |___ |__/ |  | |___ |__/    |__] |__/ |  | |    |___ [__  [__  ");
-            Console.WriteLine("|__| | \\| | |    | |___ |__/    ___] |___ |  \\  \\/  |___ |  \\    |    |  \\ |__| |___ |___ ___] ___]");
-            Console.WriteLine("---------------------------------------------------------------------------------------------------");
-            Console.WriteLine("                       Demonstration For Terraria v{0} & OTAPI v{1}                         ", version.TerrariaVersion, version.OTAPIVersion);
-            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+            Console.Title = "UnifiedServerProcess v" + typeof(Program).Assembly.GetName().Version;
+
+            Console.WriteLine(@"___________________________________________________________________________________________________");
+            Console.WriteLine(@"_  _ _  _ _ ____ _ ____ ___     ____ ____ ____ _  _ ____ ____    ___  ____ ____ ____ ____ ____ ____");
+            Console.WriteLine(@"|  | |\ | | |___ | |___ |  \    [__  |___ |__/ |  | |___ |__/    |__] |__/ |  | |    |___ [__  [__  ");
+            Console.WriteLine(@"|__| | \| | |    | |___ |__/    ___] |___ |  \  \/  |___ |  \    |    |  \ |__| |___ |___ ___] ___]");
+            Console.WriteLine(@"---------------------------------------------------------------------------------------------------");
+            Console.WriteLine(@"                       Demonstration For Terraria v{0} & OTAPI v{1}                         ", version.TerrariaVersion, version.OTAPIVersion);
+            Console.WriteLine(@"---------------------------------------------------------------------------------------------------");
 
 
             Console.Write("[USP|Info] Global initialization started... ");
