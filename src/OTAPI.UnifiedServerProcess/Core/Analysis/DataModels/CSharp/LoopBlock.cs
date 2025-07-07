5033c30dd4621cb8cc8269efc3b8aafa316ec6e4
CedaryCat <2503439479@qq.com>
2025-07-07 14:32:11 +0800

    Self-managed version-controlled work flow

---
+++ src/OTAPI.UnifiedServerProcess/Core/Analysis/DataModels/CSharp/LoopBlock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTAPI.UnifiedServerProcess.Core.Analysis.DataModels.CSharp
+{
+    // TODO: more analysis models
+    public class LoopBlock
+    {
+    }
+}
