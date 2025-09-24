96c5d3ff2c1e6922409b4929e227f272a15253af
CedaryCat <2503439479@qq.com>
2025-09-24 14:32:22 +0800

    fix: More unified check of connected clients

--- src/OTAPI.UnifiedServerProcess/Mods/NetplayMod.cs
+++ src/OTAPI.UnifiedServerProcess/Mods/NetplayMod.cs
@@ -1,41 +1,60 @@
 ﻿#pragma warning disable CS8321 // Local function is declared but never used
 #pragma warning disable CS0436 // Type conflicts with imported type
 using ModFramework;
+using Mono.Cecil;
 using Mono.Cecil.Cil;
+using Mono.Cecil.Rocks;
 using MonoMod.Cil;
 using OTAPI.UnifiedServerProcess.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 using Terraria;
 
 [Modification(ModType.PostMerge, "Netplay Send Data Check", ModPriority.Early)]
 [MonoMod.MonoModIgnore]
 void NetplayConnectionCheck(ModFwModder modder) {
+
+    List<MethodDefinition> methods = [];
+    foreach (var type in modder.Module.GetAllTypes()) { 
+        foreach (var method in type.Methods) {
+            if (!method.HasBody || method.Name == nameof(NetMessage.CheckCanSend)) {
+                continue;
+            }
+            if (method.Body.Instructions.Any(inst => inst is { Operand: MemberReference { Name: "IsConnected", DeclaringType.Name: "RemoteClient" } })) {
+                methods.Add(method);
+                continue;
+            }
+        }
+    }
+
+
     var netplay = modder.Module.GetType("Terraria.Netplay");
     var checkConnectionMDef = modder.Module.ImportReference(typeof(NetMessage).GetMethod(nameof(NetMessage.CheckCanSend))!);
-    var senddata = modder.Module.GetType("Terraria.NetMessage").GetMethod("mfwh_orig_SendData");
+    foreach (var method in methods) {
+        var cursor = method.GetILCursor();
 
-    var cursor = senddata.GetILCursor();
+        while (cursor.TryGotoNext(MoveType.Before,
+            inst => inst.MatchLdsfld("Terraria.Netplay", "Clients"),
+            inst => true,
+            inst => inst.MatchLdelemRef(),
+            inst => inst.MatchCallvirt("Terraria.RemoteClient", "IsConnected"))) {
 
+            var loadClients = cursor.Next!;
+            var loadIndex = cursor.Next!.Next!;
+            var loadElement = cursor.Next!.Next!.Next;
+            var checkConnection = cursor.Next!.Next!.Next!.Next;
 
-    while (cursor.TryGotoNext(MoveType.Before,
-        inst => inst.MatchLdsfld("Terraria.Netplay", "Clients"),
-        inst => true,
-        inst => inst.MatchLdelemRef(),
-        inst => inst.MatchCallvirt("Terraria.RemoteClient", "IsConnected"))) {
+            loadClients.OpCode = OpCodes.Nop;
+            loadClients.Operand = null;
 
-        var loadClients = cursor.Next!;
-        var loadIndex = cursor.Next!.Next!;
-        var loadElement = cursor.Next!.Next!.Next;
-        var checkConnection = cursor.Next!.Next!.Next!.Next;
+            loadElement.OpCode = OpCodes.Nop;
+            loadElement.Operand = null;
 
-        loadClients.OpCode = OpCodes.Nop;
-        loadClients.Operand = null;
-
-        loadElement.OpCode = OpCodes.Nop;
-        loadElement.Operand = null;
-
-        checkConnection.OpCode = OpCodes.Call;
-        checkConnection.Operand = checkConnectionMDef;
+            checkConnection.OpCode = OpCodes.Call;
+            checkConnection.Operand = checkConnectionMDef;
+        }
     }
 }
 
